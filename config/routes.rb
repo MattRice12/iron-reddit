19bba62de0e9bc41a73997b469672c23ac698d42
Matt Rice <mattrice12@outlook.com>
2016-07-30 19:52:31 -0500

    Fixed buttons and forms

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
   delete '/sign_out' => 'clearance/sessions#destroy', as: 'sign_out'
 
-  get '/sign_up' => 'clearance/users#new', as: 'sign_up'
+  post '/sign_up' => 'clearance/users#new', as: 'sign_up'
 
   resources :passwords,
   controller: 'clearance/passwords',
